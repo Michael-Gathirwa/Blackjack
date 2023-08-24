d4ee15d3481129927b3ee62ffc80fc4efe099278
Michael-Gathirwa <135808810+Michael-Gathirwa@users.noreply.github.com>
2023-08-24 19:36:47 +0300

    Create black.js

---
+++ black.js
@@ -0,0 +1,12 @@
+let firstCard = 3
+let secondCard = 24
+ let sum = firstCard+SecondCard
+
+if (sum<25){
+console.log("Do you Want to draw another card")
+}
+else if(sum===25){
+  console.log("You have your JACKS!")
+}
+else {
+  console.log("You are OUT!")
