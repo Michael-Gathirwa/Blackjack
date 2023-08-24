cfcb08082c4c7acd39269cec203d4da7f7892ea8
Michael-Gathirwa <135808810+Michael-Gathirwa@users.noreply.github.com>
2023-08-24 19:52:09 +0300

    Update black.js

--- black.js
+++ black.js
@@ -17,3 +17,16 @@ let age=22
 
 if (age<21){ console.log("You can not enter the Club")}
 else console.log("Welcome")
+
+
+
+let sage=100
+
+if(sage<100){
+    console.log("Not Eligeble")
+} else if(sage===100){
+    console.log("Here is your birthday card from the king")
+}
+else{
+    console.log("Not Eligible, you have already gotten one")
+}
