337ae780eaa1a8d3397ede325c757f44a5462a4e
Michael-Gathirwa <135808810+Michael-Gathirwa@users.noreply.github.com>
2023-08-29 23:57:43 +0300

    Update black.js

--- black.js
+++ black.js
@@ -6,7 +6,9 @@ let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
 function startGame(){
+ sumEl.textContent="Sum: " + sum
      if (sum<25){
      message="Do you Want to draw another card"
      }
