bcc582959c41117cd775eeb99e80e836bd7ed2bb
Michael-Gathirwa <mikekenn8791@gmail.com>
2023-09-16 12:29:28 +0300

    c

--- black.js
+++ black.js
@@ -44,10 +44,10 @@ if (sum <= 20) {
     "Great to hear",
     "michael"
  ]
-console.log(messages[0])
-console.log(messages[1])
-console.log(messages[2])
-console.log(messages[3])
-console.log(messages[4])
+// console.log(messages[0])
+// console.log(messages[1])
+// console.log(messages[2])
+// console.log(messages[3])
+// console.log(messages[4])
 
  
