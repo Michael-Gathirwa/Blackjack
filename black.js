1cf2c46196c78855c81bf4f6b67d2a532e5cae47
Michael-Gathirwa <mikekenn8791@gmail.com>
2023-09-16 13:19:19 +0300

    loop

--- black.js
+++ black.js
@@ -14,8 +14,11 @@ let sumEl = document.getElementById("sum-el")
  }
  let cardsEl=document.getElementById("cards-el")
  function renderGame(){
-    cardsEl.textContent="Cards: " + cards[0] + " " + cards[1 ]
+    cardsEl.textContent="Cards: "
 
+    for (let i=0;i<cards.length;i++){
+        cardsEl.textContent += cards[i] +" "
+    }
     
     sumEl.textContent="Sum:" + sum
 if (sum <= 20) {
